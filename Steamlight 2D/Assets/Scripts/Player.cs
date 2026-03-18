6f75eda2ada52b1d4d5f2cea2efd6a6f27fb67d9
sparKlZJuNiO <jrrossjob@gmail.com>
2026-03-18 12:41:14 +0000

    gded

--- Steamlight 2D/Assets/Scripts/Player.cs
+++ Steamlight 2D/Assets/Scripts/Player.cs
@@ -8,11 +8,17 @@ public class Player : MonoBehaviour
 {
     GameObject plr;
     private Vector2 moveInput;
-    Animator anim;
+ 
    [SerializeField] Rigidbody2D rb;
    [SerializeField] float speed = 0.05f;
    bool isMoving;
-    bool isPlaying = false;
+   bool isPlaying = false;
+    bool isPaused = false;
+    GameObject[] NPCs;
+
+    [Header("Animator")]
+    Animator playerAnim;
+   [SerializeField] Animator uiAnim;
 
     [Header("UI")]
     [SerializeField] GameObject canvas;
@@ -23,11 +29,11 @@ public class Player : MonoBehaviour
     private void Awake() // Best for initializing variables before the game is loaded
     {
         plr = this.GetComponent<GameObject>();
-        anim = GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
     }
     void Start() // Best for initializing variables
     {
-
+        NPCs = GameObject.FindGameObjectsWithTag("NPC");
     }
 
     // Update is called once per frame
@@ -43,22 +49,22 @@ public class Player : MonoBehaviour
         if (moveInput.x > 0)
         {
             Debug.Log("Right");
-            anim.SetBool("sideways", true);
-            anim.SetBool("forward", false);
+            playerAnim.SetBool("sideways", true);
+            playerAnim.SetBool("forward", false);
             this.GetComponent<SpriteRenderer>().flipX = true;
         }
         if (moveInput.x < 0)
         {
             Debug.Log("Left");
-            anim.SetBool("sideways", true);
-            anim.SetBool("forward", false);
+            playerAnim.SetBool("sideways", true);
+            playerAnim.SetBool("forward", false);
             this.GetComponent<SpriteRenderer>().flipX = false;
         }
         if (-moveInput.y > 0)
         {
             Debug.Log("Below");
-            anim.SetBool("sideways", false);
-            anim.SetBool("forward", true);
+            playerAnim.SetBool("sideways", false);
+            playerAnim.SetBool("forward", true);
             this.GetComponent<SpriteRenderer>().flipX = false;
         }
     }
@@ -75,32 +81,49 @@ public class Player : MonoBehaviour
 
     public void PauseButton()
     {
-        Time.timeScale = 0;
-        pauseButton.GetComponent<Image>().enabled = false;
-        pauseButton2.GetComponent<Image>().enabled = true;
+        //Time.timeScale = 0;
+        speed = 0;
+       pauseButton.GetComponent<Image>().enabled = false;
+      pauseButton2.GetComponent<Image>().enabled = true;
+        uiAnim.SetBool("pause", true );
         pauseScreen.SetActive(true);
+        isPaused = true;
+        playerAnim.SetBool("sideways", false);
+        playerAnim.SetBool("forward", false);
+        playerAnim.SetBool("move", false);
+        foreach (GameObject gameObject in NPCs)
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
+        }
     }
     public void unPauseButton()
     {
-        Time.timeScale = 1;
-        pauseButton.GetComponent<Image>().enabled = true;
+        //Time.timeScale = 1;
+       pauseButton.GetComponent<Image>().enabled = true;
         pauseButton2.GetComponent<Image>().enabled = false;
         pauseScreen.SetActive(false);
+        uiAnim.SetBool("pause", false);
+        isPaused = false;
+        speed = 0.05f;
+        foreach (GameObject gameObject in NPCs)
+        {
+            gameObject.GetComponent<Animator>().enabled = true;
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
-        if (isPlaying == true)
+        if (isPlaying == true && !isPaused)
         {
             moveInput = ctx.ReadValue<Vector2>();
 
             if (ctx.performed)
             {
-                anim.SetBool("move", true);
+                playerAnim.SetBool("move", true);
             }
             else
             {
-                anim.SetBool("move", false);
+                playerAnim.SetBool("move", false);
             }
         }
     }
